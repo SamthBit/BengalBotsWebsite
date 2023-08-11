d0ab880b8d473cac8d367f1ff5656c7eb9b33e5c
BakedPotato101 <jackson.baker7562@gmail.com>
2023-08-11 00:11:38 -0500

    added member cards on /members

--- src/routes/Members/+page.svelte
+++ src/routes/Members/+page.svelte
@@ -1,10 +1,31 @@
-<script>
+<script lang="ts">
+	import supabase from '$lib/supaBaseConfig.js';
+	import Icon from '@iconify/svelte';
 	import { onMount } from 'svelte';
-	import supabase from '$lib/supaBaseConfig';
-	let members = [];
+	let members: any[] = [];
 	onMount(async () => {
-		let { data, error } = await supabase.from('Members').select();
+		const { data, error } = await supabase.from('Members').select('*');
+		if (error) {
+			throw error;
+		}
 		members = data;
-		console.table(data);
 	});
 </script>
+
+{#if members}
+	<center>
+		{#each members as member}
+			<div class="card inline-block m-10 max-w-xs">
+				<header class="card-header"><h1>{member.Name}</h1></header>
+				<section class="p-4">
+					<Icon icon="fluent:person-24-filled" width="200px" color="firebrick"/>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore autem rerum ad totam vero
+					voluptatum soluta atque eum.
+				</section>
+				<footer class="card-footer"><h2>{member.Role}</h2></footer>
+			</div>
+		{/each}
+	</center>
+{:else}
+	<h1>Loading members...</h1>
+{/if}
